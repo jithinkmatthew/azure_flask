10dc179875d159510ca1bc9896e1e413eb5e1561
Jithin Mathew <7393772+jithinkmatthew@users.noreply.github.com>
2025-08-13 18:38:51 +1200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Welcome to AgriHire!</p>"
+    return "<p>Kia Ora, Welcome to AgriHire!</p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
